de87eb7120017caa04d76891358ef91298869800
colingalvin <colingalvin@ymail.com>
2020-10-16 13:19:23 -0500

    Complete GlobalRouting Action Filter.

--- MyTrashCollector/Startup.cs
+++ MyTrashCollector/Startup.cs
@@ -12,6 +12,9 @@ using MyTrashCollector.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using MyTrashCollector.ActionFilters;
 
 namespace MyTrashCollector
 {
@@ -35,6 +38,12 @@ namespace MyTrashCollector
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultUI()
                 .AddDefaultTokenProviders();
+
+            services.AddScoped<ClaimsPrincipal>(s => s.GetService<IHttpContextAccessor>().HttpContext.User);
+            services.AddControllers(config =>
+            {
+                config.Filters.Add(typeof(GlobalRouting));
+            });
 
             services.AddControllersWithViews();
             services.AddRazorPages();
